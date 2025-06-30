ef5d3c939c4e6d50488773c9b7ba4a942957234e
Vladimir <kremench@gmail.com>
2025-06-30 19:19:58 +0300

    add index.js

--- src/server.js
+++ src/server.js
@@ -4,35 +4,41 @@ import dotenv from 'dotenv';
 import pino from 'pino';
 import cors from 'cors';
 
-dotenv.config();
 const PORT = process.env.PORT || 3000;
+dotenv.config();
 
-const app = express();
+export const setupServer = () => {
+  const app = express();
 
-app.use(cors());
-app.use(express.json());
+  app.use(express.json());
+  app.use(cors());
 
-const logger = pino({
-  level: 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
+  const logger = pino({
+    transport: {
+      target: 'pino-pretty',
     },
-  },
-});
+  });
+  app.use(pinoHttp({ logger }));
 
-app.use(pinoHttp({ logger }));
+  app.get('/', (req, res) => {
+    req.log.info('Обробка GET запиту');
+    res.json({ message: 'Welcome to the Server' });
+  });
 
-function setupServer(req, res) {
-  req.log.info();
-}
+  app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
 
-app.get('/', (req, res) => {
-  req.log.info('Обробка GET запиту');
-  res.json({ message: 'Welcome to the Server' });
-});
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
 
-app.listen(PORT, () => {
-  logger.info(`Server is runing on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    logger.info(`Server is runing on port ${PORT}`);
+  });
+};
