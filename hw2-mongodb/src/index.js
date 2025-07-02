fae57ac7c34881150b9bdb0ef9aec89aa0f5aabf
Vladimir <kremench@gmail.com>
2025-07-02 22:30:41 +0300

    updata

--- hw2-mongodb/src/index.js
+++
@@ -1,8 +0,0 @@
-import { initMongoDB } from './db/initMongoConnection.js';
-import { setupServer } from './server.js';
-
-const bootstrap = async () => {
-  await initMongoDB();
-  setupServer();
-};
-bootstrap();
