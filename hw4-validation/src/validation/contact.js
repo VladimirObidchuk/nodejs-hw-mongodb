eeb994e6e39e2250612c92283f0ae2b6829d26ac
Vladimir <kremench@gmail.com>
2025-07-18 12:00:28 +0300

    fix errors report validation

--- hw4-validation/src/validation/contact.js
+++ hw4-validation/src/validation/contact.js
@@ -3,7 +3,10 @@ import Joi from 'joi';
 export const contactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().min(3).max(20),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .min(3)
+    .max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string()
     .valid('work', 'home', 'personal')
@@ -14,7 +17,10 @@ export const contactSchema = Joi.object({
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20),
-  email: Joi.string().min(3).max(20),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .min(3)
+    .max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string()
     .valid('work', 'home', 'personal')
